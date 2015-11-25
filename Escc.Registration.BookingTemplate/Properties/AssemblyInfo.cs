3012543a561c1300b99d6fd05bf0cfaf11f67e8f
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-11-25 14:20:32 +0000

    Initial commit

---
+++ Escc.Registration.BookingTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Escc.Registration.BookingTemplate")]
+[assembly: AssemblyDescription("Supporting code for the East Sussex County Council implementation of stopford.com")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("East Sussex County Council")]
+[assembly: AssemblyProduct("Escc.Registration.BookingTemplate")]
+[assembly: AssemblyCopyright("East Sussex County Council")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5892a133-ede9-4161-a770-512a2cda4ef7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
+[assembly: CLSCompliant(true)]
---
+++ README.md
@@ -0,0 +1,10 @@
+# Escc.Registration.BookingTemplate
+
+An HTML template for East Sussex County Council registration service's implementation of the [stopford.com](http://stopford.com/) booking system.
+
+This template is published on our website, and downloaded regularly by Stopford for use in their product.
+
+## Development setup steps
+
+1. From an Administrator command prompt, run `app-setup-dev.cmd` to set up a site in IIS
+2. Build the solution
